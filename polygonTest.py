4267337f2c1201b55532ee89114ddf5ec9e1bb4e
MChoi-git <mattchoi427@gmail.com>
2021-04-16 02:24:26 -0400

    DBsing init func added to new polygon.io stuff

--- polygonTest.py
+++ polygonTest.py
@@ -1,6 +1,26 @@
 # This is a test file to practice polygon.io REST API
 
 from polygon import RESTClient
+from get_all_tickers import get_tickers as gt
+import os
+import pandas as pd
+import datetime
+
+def init_root_db(tickerList):
+    idList = []
+    dateList = []
+    print("Root db ids being created...")
+    for i in range(len(tickerList)):
+        idList.append(i)
+    print("Root db ids have been successfully created.")
+    print("Root db dates being created...")
+    for i in range(len(tickerList)):
+        dateList.append(datetime.date.today())
+    print("Root db dates have been successfully created.")
+    data = list(zip(idList,tickerList,dateList))
+    df = pd.DataFrame(data,columns=['id','ticker','lastUpdated'])
+    df.to_csv(path_or_buf="Root_Database.csv")
+    return 1
 
 def main():
     key = 'PjeqU9zauMH9o49WYWurfZslqfY8HpF7'
@@ -22,5 +42,23 @@ def main():
             print(f"MSFT revenues in USD is {resp.results[i].get('revenuesUSD')} as reported on {resp.results[i].get('reportPeriod')}.")
             print(f"MSFT operating income is {resp.results[i].get('operatingIncome')} as reported on {resp.results[i].get('reportPeriod')}.")
             print('*'*50)
+
+        print("Testing completed for stock financials. Beginning testing for databasing.")
+        print("="*50)
+        
+        # Get a list of all tickers
+        list_of_all_tickers = gt.get_tickers(NYSE=True,NASDAQ=True,AMEX=False)
+        
+        DB_EXISTENCE_FLAGS = [False,False,False]
+
+        if os.path.isfile("Root_Database.csv"):
+            print("Root DB found, aborting init operation.")
+        else:
+            if(init_root_db(list_of_all_tickers)):
+                DB_EXISTENCE_FLAGS[0] = True
+                print("Root DB has been successfully initialized.")
+            else:
+                print("Root DB initialization has failed.")
+
 if __name__ == '--main__':
     main()
