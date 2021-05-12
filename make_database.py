b7c94658f8ec666f84b41e9f646f112203af0bc6
MChoi-git <mattchoi427@gmail.com>
2021-05-12 15:46:33 -0400

    Finished make file for dbs

--- make_database.py
+++ make_database.py
@@ -1,9 +1,11 @@
+#Creates primary, ohlc, and financials databases in docker container
+
 #Import 
 import sqlalchemy as db
 import time
 from sqlalchemy.orm import Session
 from sqlalchemy import MetaData
-from sqlalchemy import Table, Column, Integer, String, Date
+from sqlalchemy import Table, Column, Integer, String, Date, Float
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import registry
 from sqlalchemy.orm import relationship
@@ -41,6 +43,7 @@ class PrimaryDB(Base):
     ticker = Column(String(8))
 
     ohlc = relationship("OHLC", back_populates="primarydb")
+    financials = relationship("Financials", back_populates="primarydb")
 
     def __repr__(self):
         return f"Primary(id={self.id!r}, name={self.name!r}, ticker={self.ticker!r})"
@@ -51,7 +54,7 @@ class OHLC(Base):
     
     #Columns definition
     id = Column(Integer, primary_key=True)
-    data = Column(Date, nullable=False)
+    date = Column(Date, nullable=False)
     dayopen = Column(Integer, nullable=False)
     dayhigh= Column(Integer, nullable=False)
     daylow = Column(Integer, nullable=False)
@@ -60,6 +63,28 @@ class OHLC(Base):
 
     primary = relationship("PrimaryDB", back_populates="ohlc")
 
+    def __repr__(self):
+        return f"OHLC(id={self.id!r}, date={self.date!r}, dayopen={self.dayopen!r}, dayhigh={self.dayhigh!r}, daylow={self.daylow!r}, dayclose={self.dayclose!r}, dayvolume={self.dayvolume!r})"
+
 #Create class for financials TBD
+class Financials(Base):
+    __tablename__ = 'financials'
+
+    #Columns definition
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    debt_to_equity_ratio = Column(Float)
+    dividend_yield = Column(Float)
+    earnings_per_diluted_share = Column(Float)
+    gross_profit = Column(Integer)
+    net_income = Column(Integer)
+    price_to_earnings_ratio = Column(Float)
+    revenues_usd = Column(Integer)
+
+    primary = relationship("PrimaryDB", back_populates="financials")
+
+    def __repr__(self):
+        return f"Financials(id={self.id!r}, date={self.date!r}, debt_to_equity_ratio={self.debt_to_equity_ratio!r}, dividend_yield={self.dividend_yield!r}, earnings_per_diluted_share={self.earnings_per_diluted_share!r}, gross_profit={self.gross_profit!r}, net_income={self.net_income!r}, price_to_earnings_ratio={self.price_to_earnings_ratio!r}, revenues_usd={self.revenues_usd!r})"
+
 
 Base.metadata.create_all(engine)
