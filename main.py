f9fd7c01e0eb3ffa0eafcfc907638cf02f78794c
MChoi-git <mattchoi427@gmail.com>
2021-05-17 22:13:10 -0400

    main now controls all. Began CL integration

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 #Import 
 import sqlalchemy as db
 import time
+import sys
 from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import MetaData
 from sqlalchemy import Table, Column, Integer, String, Date, Float
 from sqlalchemy import ForeignKey
@@ -11,6 +13,7 @@ from sqlalchemy.orm import registry
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import insert
+import getpass
 
 #Set config for connection to docker
 config = {
@@ -86,5 +89,47 @@ class Financials(Base):
     def __repr__(self):
         return f"Financials(id={self.id!r}, date={self.date!r}, debt_to_equity_ratio={self.debt_to_equity_ratio!r}, dividend_yield={self.dividend_yield!r}, earnings_per_diluted_share={self.earnings_per_diluted_share!r}, gross_profit={self.gross_profit!r}, net_income={self.net_income!r}, price_to_earnings_ratio={self.price_to_earnings_ratio!r}, revenues_usd={self.revenues_usd!r})"
 
-
+#Create the schema
 Base.metadata.create_all(engine)
+
+#Launch gui
+
+#Check user credentials
+userCred = input("Enter username: ")
+pwdCred = getpass.getpass(prompt="Enter password: ")
+if(pwdCred == db_pwd and userCred == db_user):
+    print("Login successful. Welcome.")
+else:
+    print("Invalid login credentials. Exiting program.")
+    sys.exit()
+
+#Create a session
+Session = sessionmaker(bind=engine)
+
+#Prompt for commands
+strPrompt = input("What would you like to do? Press help to browse all commands.")
+args = split(strip(strPrompt))
+print("User selected to do: " + str(args))
+
+#Print the help information
+if(args[0] == "help"):
+
+#Manually add an entry
+elif(args[0] == "add"):
+
+#Manually remove an entry
+elif(args[0] == "remove"):
+
+#Populate the database from csv file
+#Args: filename/path
+elif(args[0] == "populate"):
+
+#Add entries to database
+#Args: timeframe, ticker
+elif(args[0] == "update"):
+
+#Close the program
+elif(args[0] == "exit"):
+    print("Exiting program. Goodbye.")
+    sys.exit()
+
